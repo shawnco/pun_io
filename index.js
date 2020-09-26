453dbf82a6c277e392daeaf6f31aaa892776081d
Shawn Contant <shawnc366@gmail.com>
2020-09-26 13:57:08 -0500

    Stream markers

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const {StaticAuthProvider} = require('twitch-auth');
 const {ChatClient} = require('twitch-chat-client');
 const config = require('./config.json');
 const twitchBot = require('twitch-bot');
+const Sequelize = require('sequelize');
+const path = require('path');
+
+const db = new Sequelize('punio', 'pun_io', 'password', {
+	dialect: 'sqlite',
+	storage: path.join(__dirname) + '/punio.db'
+});
+
+
 console.log(config)
 app.listen(process.env.PORT, () => {
 	console.log('Pun_IO is live');
@@ -32,7 +41,8 @@ bot.on('message', chatter => {
 	if (message.toLowerCase() == 'hi pun_io') {
 		bot.say(`Hello ${username}!`, channel);
 	} else {
-		const command = message.split(' ')[0];
+		const parts = message.split(' ');
+		const command = parts[0];
 		if (command == '!punio') {
 			bot.say('Pun_IO is a Twitch bot by shawntc. For feedback you can whisper shawntc on Twitch or contact him on Twitter @shawntco', channel);
 		}
@@ -44,12 +54,52 @@ bot.on('message', chatter => {
 					Accept: 'application/json'
 				}
 			};
-			//rp(punOptions).then(res => bot.say(res.joke, channel));
 			rp(punOptions).then(res => {
 				const {joke} = JSON.parse(res);
 				bot.say(joke, channel);
 			});
 		
+		}
+		if (command == '!punmark') {
+			client.helix.users.getUserByName(channel).then(user => {
+				const id = user.id;
+				client.helix.streams.getStreamByUserId(user.id).then(stream => {
+					//console.log(stream.);
+					const {started_at} = stream._data;
+					const now = (new Date()).getTime();
+					const started = (new Date(started_at)).getTime();
+					const len = (now - started) / 1000;
+					const seconds = Math.round(len);
+					client.helix.videos.getVideosByUser(id).then(videos => {
+						const link = `${videos.data[0]._data.url}?t=${seconds}s`;
+						console.log(link, channel);
+						db.query('INSERT INTO marks (link, channel) VALUES (:link, :channel)', {
+							QueryType: db.QueryTypes.INSERT,
+							replacements: { link, channel },
+							logging: console.log
+						})
+						.then(result => console.log(result[1].lastID))
+						.catch(err => console.log('Oof', err));
+					});
+				});
+			});
+		}
+		if (command == '!punmarkget') {
+			const id = parts[1];
+			db.query('SELECT * FROM marks WHERE id = :id', {
+				QueryType: db.QueryTypes.SELECT,
+				replacements: { id },
+				logging: console.log
+			})//.then(result => console.log(result[0][0].link))
+			.then(result => {
+				if (result[0][0]) {
+					const {link, channel} = result[0][0];
+					console.log(`(${channel}): ${link}`);
+				} else {
+					console.log('Not found');
+				}
+			})
+			.catch(err => console.log('Ouch', err));
 		}
 	}
 });
